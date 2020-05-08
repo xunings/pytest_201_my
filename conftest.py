0040759419e890268657ee42659219d0beddc21b
shaoxuning <shaoxuning@corp.netease.com>
2020-05-08 12:07:29 +0800

    Add comments

--- conftest.py
+++ conftest.py
@@ -2,6 +2,10 @@ import pytest
 
 @pytest.fixture
 def my_fixture():
+    """
+    This is the docstring for the my_fixture fixture.
+    This will show up in pytest --fixtures
+    """
     return 42
 
 @pytest.fixture
